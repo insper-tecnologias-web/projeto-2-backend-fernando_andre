8571946cf3b8c0f8cf425944d6b4e0a09d88af1b
andre <andref9@al.insper.edu.br>
2024-04-30 13:39:13 -0300

    feito

--- notes/models.py
+++ notes/models.py
@@ -24,6 +24,6 @@ class Produto(models.Model):
 
 class Moeda(models.Model):
     nome = models.CharField(max_length=200)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    preco = models.DecimalField(max_digits=10, decimal_places=8, default=0.0)
     def __str__(self):
         return str(self.id) + '.' + ' ' + str(self.nome) 
