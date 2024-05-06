a1290ed65234752ac9bb52a6eeb670fcc2efdd55
andre <andref9@al.insper.edu.br>
2024-05-05 22:43:42 -0300

    deploy

--- getit/settings.py
+++ getit/settings.py
@@ -107,11 +107,19 @@ CORS_ALLOWED_ORIGINS = [
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='',
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
